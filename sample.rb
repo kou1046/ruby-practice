6b1e985f6832d03ef6e10d8d2d056c6dbebb31c4
iwashiro <iwashiro0517@yahoo.co.jp>
2023-06-27 02:44:26 +0900

    行列計算処理の追加（途中）

--- sample.rb
+++ sample.rb
@@ -6,11 +6,12 @@ require 'pycall/import'
 module Py
   extend PyCall::Import
   pyimport('matplotlib.pyplot', as: :plt)
+  pyimport('numpy', as: :np)
 end
 
 # 正の座標を表す
 class Point
-  attr_accessor :x, :y
+  attr_reader :x, :y
 
   def initialize(x_cor, y_cor)
     raise ArgumentError, '負の数は受け付けない' if x_cor.negative? || y_cor.negative?
@@ -26,7 +27,7 @@ end
 
 # 壁を表現する
 class Wall
-  attr_accessor :point1, :point2
+  attr_reader :point1, :point2
 
   def initialize(point1, point2)
     @point1 = point1.freeze
@@ -69,31 +70,29 @@ class Wall
     @point1 == other.point1 && @point2 == other.point2
   end
 
-  def reflect!
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices)
     raise NotImplementedError, 'このメソッドを継承してオーバライドする必要がある'
   end
 end
 
 # 右壁
 class RightWall < Wall
-  def reflect!
-    puts 'sample'
-  end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 左壁
 class LeftWall < Wall
-  def reflect!; end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 上壁
 class TopWall < Wall
-  def reflect!; end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 下壁
 class BottomWall < Wall
-  def reflect!; end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 角
@@ -102,45 +101,36 @@ class CornerWall
     @point = point.freeze
   end
 
-  def xs
-    [@point.x]
-  end
-
-  def ys
-    [@point.y]
-  end
-
-  def reflect!
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices)
     raise NotImplementedError, 'このメソッドを継承してオーバライドする必要がある'
   end
 end
 
 # 右上
 class RightTopCorner < CornerWall
-  def reflect!; end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 左上
 class LeftTopCorner < CornerWall
-  def reflect!; end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 右下
 class RightBottomCorner < CornerWall
-  def reflect!; end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 左下
 class LeftBottomCorner < CornerWall
-  def reflect!; end
+  def reflect!(inplaced_value, value, pre_value, x_indice, y_indices); end
 end
 
 # 障害物
 class Obstacle
-  include Enumerable
-
   def initialize(walls, wave_pass_through: true)
-    @walls = initialize_(walls, wave_pass_through: wave_pass_through).freeze
+    @walls = walls.freeze
+    @corners = get_corners(wave_pass_through: wave_pass_through).freeze
   end
 
   def xs
@@ -151,16 +141,19 @@ class Obstacle
     @walls.map(&:ys).flatten
   end
 
-  def each(&block)
+  def each_wall(&block)
     @walls.each(&block)
+  end
+
+  def each_corner(&block)
+    @corners.each(&block)
   end
 
   private
 
-  def initialize_(walls, wave_pass_through: true)
-    loop_walls = walls + [walls[0]]
+  def get_corners(wave_pass_through: true)
+    loop_walls = @walls + [@walls[0]]
     loop_walls.each_cons(2).each_with_object([]) do |(wall, next_wall), collection|
-      collection << wall.freeze
       corner = if wave_pass_through
                  get_internal_corner(wall, next_wall)
                else
@@ -193,14 +186,16 @@ end
 
 # 格子
 class Grid
-  def initialize(width, height, grid_side)
+  attr_reader :side, :width, :height
+
+  def initialize(width, height, side)
     @width = width
     @height = height
-    @grid_side = grid_side
+    @side = side
   end
 
   def calculate_grid_num(num)
-    (num / @grid_side).floor
+    (num / @side).floor
   end
 
   def row_num
@@ -209,6 +204,28 @@ class Grid
 
   def col_num
     calculate_grid_num @height
+  end
+
+  def wall_indices(wall)
+    if wall.horizontal?
+      xmin, xmax = wall.xs.sort.map { |cor| calculate_grid_num(cor) }
+      x_indices = (xmin...xmax).to_a
+      y = wall.ys[0]
+      y_indices = [y] * x_indices.length
+    else
+      ymin, ymax = wall.ys.sort.map { |cor| calculate_grid_num(cor) }
+      y_indices = (ymin...ymax).to_a
+      x = wall.xs[0]
+      x_indices = [x] * y_indices.length
+    end
+    [x_indices, y_indices]
+  end
+
+  def corner_index(corner)
+    x_index = calculate_grid_num(corner.point.x)
+    y_index = calculate_grid_num(corner.point.y)
+
+    [x_index, y_index]
   end
 end
 
@@ -223,14 +240,41 @@ class WaveFactory
     @grid = Grid.new(x_max, y_max, h)
     @obstacles = obstacles
     @dt = dt
+    @wave_value = Py.np.zeros([@grid.row_num, @grid.col_num])
+    @pre_wave_value = Py.np.zeros([@grid.row_num, @grid.col_num])
   end
 
-  def create; end
+  def create
+    value_r = Py.np.roll(@wave_value, -1, 1)
+    value_l = Py.np.roll(@wave_value, 1, 1)
+    value_b = Py.np.roll(@wave_value, -1, 0)
+    value_t = Py.np.roll(@wave_value, 1, 0)
+    new_value = 2 * @wave_value - @pre_wave_value + alpha * (value_l + value_r + value_b + value_t - 4 * @wave_value)
+    @obstacles.each do |obstacle|
+      obstacle.each_wall { |wall| wall.reflect!(new_value, @wave_value, @pre_wave_value, *@grid.wall_indices(wall)) }
+      obstacle.each_corner do |corner|
+        corner.reflect!(new_value, @wave_value, @pre_wave_value, *@grid.corner_index(corner))
+      end
+    end
+    Wave.new(new_value.freeze)
+    @pre_wave_value = @wave_value.freeze
+    @wave_value = new_value.freeze
+  end
+
+  private
+
+  def alpha
+    te (@dt / @grid.side)**2
+  end
 end
 
 # 波
 class Wave
-  def initialize; end
+  attr_reader :value
+
+  def initialize(value)
+    @value = value.freeze
+  end
 end
 
 point1 = Point.new(0, 0)
@@ -264,3 +308,4 @@ obstacle2 = Obstacle.new(wall_list2, wave_pass_through: false)
 obstacles = [obstacle, obstacle2]
 
 wavefactory = WaveFactory.new(obstacles, 0.01, 0.005)
+wavefactory.create
